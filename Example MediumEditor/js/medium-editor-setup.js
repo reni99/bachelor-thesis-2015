4848582c7fce4d3bcc79769e8f66afb1e845ee18
reni99 <renato.stauffer90@googlemail.com>
2015-03-26 11:16:29 +0100

    Rearange structure of extension

--- Example MediumEditor/js/medium-editor-setup.js
+++ Example MediumEditor/js/medium-editor-setup.js
@@ -1,25 +1,40 @@
-        function MetaEditorExtension() {
-          this.parent = true;
-          this.button = document.createElement('button');
-          this.button.className = 'medium-editor-action';
-          this.button.innerText = 'Meta data';
-          this.button.onclick = this.onClick.bind(this);
-        }
+/*jslint browser: true*/
+/*global $, jQuery, MetaEditorView, MetaEditorPresenter, MediumEditor*/
 
-        MetaEditorExtension.prototype.getButton = function() {
-          return this.button;
-        };
+  function MetaEditorExtension() {
+    "use strict";
+    var view = new MetaEditorView(),
+    presenter = new MetaEditorPresenter(view);
 
-        var view = new MetaEditorView();
-        
-        MetaEditorExtension.prototype.onClick = function() {
-          view.toggle();
-          var presenter = new MetaEditorPresenter(view);
-        };
+    this.parent = true;
+    this.button = document.createElement('button');
+    this.button.className = 'medium-editor-action';
+    this.button.innerText = 'Meta data';
+    this.button.onclick = this.onClick.bind(this);
+    
+    this.getView = function(){
+      return view;
+    };
 
-        var editor = new MediumEditor('.editable', {
-        	buttons: ['extension'],
-        	extensions: {
-        		extension: new MetaEditorExtension()
-        	}
-        });
+    this.getPresenter = function(){
+      return presenter;
+    };
+
+  }
+
+  MetaEditorExtension.prototype.getButton = function() {
+    "use strict";
+    return this.button;
+  };
+  
+  MetaEditorExtension.prototype.onClick = function() {
+    "use strict";
+    this.getView().toggle();
+  };
+
+  var editor = new MediumEditor('.editable', {
+  	buttons: ['extension'],
+  	extensions: {
+  		extension: new MetaEditorExtension()
+  	}
+  });
