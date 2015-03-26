4cf81ba08914ca3221e35139a150aa7a60834a3e
reni99 <renato.stauffer90@googlemail.com>
2015-03-26 17:26:03 +0100

    Add meta editor

---
+++ Example Medium-js/js/meta-editor-view.js
@@ -0,0 +1,46 @@
+
+/*jslint browser: true*/
+/*global $, jQuery*/
+
+function MetaEditorView(){
+	"use strict";
+
+	var toggleState = false ;
+
+	this.setToggleState = function(newToggleState){
+		if(typeof newToggleState === 'boolean'){
+			toggleState = newToggleState;
+			return true;
+		}
+		return false;
+	};
+
+	this.getToggleState = function(){
+		return toggleState;
+	};
+
+	this.toggle = function(){
+		if(!this.getToggleState()){
+			$('body').append('<aside class="metaEditor"><h3>Meta Editor</h3><input type="text" placeholder="Subject"/><input type="text" id="predicateInput" placeholder="Predicate"/><button>i</button><input type="text" placeholder="Object"/><button id="saveTripleButton" type="button">Save</button></aside>');
+			this.setToggleState(true);
+		}else{
+			$('.metaEditor').detach();
+			this.setToggleState(false);
+		}
+		return;
+	};
+
+	//Handlers for presenter
+	this.addPredicateInputHandler = function(handler){
+		$('#predicateInput').keyup(handler);
+	};
+
+	this.addSaveTripleHandler = function(handler){
+		$('#saveTripleButton').click(handler);
+	};
+
+	this.addSubjectInputHandler = function(handler){
+		//TODO: Implement
+	};
+
+}
